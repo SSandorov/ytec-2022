26fd711bacf8af027915df00b83b5ca1aca75612
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-11-09 17:44:49 +0100

    first models and admin config

--- soccer/models.py
+++ soccer/models.py
@@ -1,3 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Player(models.Model):
+    player_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.player_text
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=200)
+    players = models.ManyToManyField(Player)
+
+    def __str__(self):
+        return self.team_name
+
+class Match(models.Model):
+    teams = models.ManyToManyField(Team)
+    points_1 = models.IntegerField(default=0)
+    points_2 = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+
+    def was_played_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=7)
