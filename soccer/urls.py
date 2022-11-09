edcd652163f43afae20dafabbfc3b51d4506932c
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-11-09 19:31:30 +0100

    results view

--- soccer/urls.py
+++ soccer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:team_id>/', views.results, name='results')
 ]
--- soccer/views.py
+++ soccer/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Player, Team, Match
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Soccer app")
+    team_list = Team.objects.order_by('id')
+    # output = ', '.join([t.team_name for t in team_list])
+    # return HttpResponse(output)
+    context = {'team_list': team_list}
+    return render(request, 'soccer/index.html', context)
+
+def results(request, team_id):
+    team = get_object_or_404(Team, pk=team_id)
+    return render(request, 'soccer/results.html', {'team': team})
